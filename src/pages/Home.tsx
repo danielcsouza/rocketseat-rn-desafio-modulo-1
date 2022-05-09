3471f91dd9c09ee60fe4ea66c4e0edfcc207c9ed
Daniel Carlos <daniel.carlos.souza@gmail.com>
2022-05-09 13:50:43 -0300

    resolucao desafio 1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,14 +10,43 @@ export function Home() {
 
   function handleAddTask(newTaskTitle: string) {
     //TODO - add new task
+    //criando objeto
+     const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    };
+
+    //adicionando no estado
+    setTasks(old =>[...old, data]);
+
   }
 
   function handleToggleTaskDone(id: number) {
     //TODO - toggle task done if exists
+    //pega o item que estou procurando
+    let task = tasks.find(s=>s.id == id);
+
+    //verifico se acho algo pq pode vir undefined
+    if (task)
+    {
+      task.done = !task.done; // mudo o status de concluido para nao concluido
+    }
+
+    //atualizo novamente as tasks pegando as que ja tem e adicionando a nova alteracao
+    //com principio de imutabilidade
+    const updatedTasks = tasks.map(task => ({...task }))
+
+    //mando atualizar o state
+    setTasks(updatedTasks);
+    
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    //criando um novo estado do Array de tasks com todos
+    //menos o que o id que foi passado.
+    setTasks(geral => geral.filter(s=>s.id !== id))
   }
 
   return (
